24c380504eda20a9a97549ec7c1e3c26f8c35b53
Aayush <43297314+Aayush9029@users.noreply.github.com>
2022-04-16 16:16:42 -0400

    Website 1 of many to come

---
+++ js/confetti.js
@@ -0,0 +1,25 @@
+let end = Date.now() + (8 * 1000);
+
+// go Buckeyes!
+var colors = ['#ff0037', '#e3e0ff'];
+
+(function frame() {
+    confetti({
+        particleCount: 2,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 },
+        colors: colors
+    });
+    confetti({
+        particleCount: 2,
+        angle: 120,
+        spread: 85,
+        origin: { x: 1 },
+        colors: colors
+    });
+
+    if (Date.now() < end) {
+        requestAnimationFrame(frame);
+    }
+}());
